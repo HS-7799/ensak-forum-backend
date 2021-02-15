80561c14a36e92a478b531fbfa2fed7cd5d65c95
hamza elharsi <hamza.elharsi77@gmail.com>
2021-02-15 01:02:06 +0100

    final touches

--- src/main/java/com/forum/ensak/controllers/AuthentificationController.java
+++ src/main/java/com/forum/ensak/controllers/AuthentificationController.java
@@ -6,18 +6,16 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import javax.validation.Valid;
-
 
 import org.apache.commons.validator.routines.EmailValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import com.forum.ensak.models.ERole;
@@ -119,6 +117,65 @@ public class AuthentificationController {
 		Set<String> strRoles = signUpRequest.getRole();
 		Set<Role> roles = new HashSet<>();
 
+		Role userRole = roleRepository.findByName(ERole.ROLE_ETUDIANT)
+				.orElseThrow(() -> new RuntimeException("Role is not found."));
+		roles.add(userRole);
+
+		user.setRoles(roles);
+		userRepository.save(user);
+		return ResponseEntity.ok(user.getId());
+	}
+
+
+	@PostMapping("/admin")
+	@PreAuthorize("hasRole('ADMIN')")
+	public ResponseEntity<?> adminCreate(@RequestBody SignupRequest signUpRequest) {
+		signUpRequest.setIs_Completed(false);
+		String errors = "";
+		if(!EmailValidator.getInstance().isValid(signUpRequest.getEmail()))
+		{
+			errors += "Email is not valid!,";
+
+		}
+
+		if (userRepository.existsByUsername(signUpRequest.getUsername())) {
+			errors += "Username is already in use!,";
+		}
+
+		if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+			errors += "Email is already in use!,";
+		}
+
+		if(signUpRequest.getPassword().length() < 6 || signUpRequest.getPassword().length() > 40)
+		{
+			errors += "password must be less than 40 and greater than 6,";
+		}
+
+		if(signUpRequest.getUsername().trim().length() < 3  || signUpRequest.getUsername().trim().length() > 20)
+		{
+			errors += "username must be less than 40 and greater than 3,";
+		}
+
+		if(signUpRequest.getPassword().trim().length() == 0 || signUpRequest.getName().trim().length() == 0 || signUpRequest.getUsername().trim().length() == 0 || signUpRequest.getEmail().trim().length() == 0)
+		{
+			errors += "All field are required,";
+		}
+		if(errors.length() > 0)
+		{
+			return ResponseEntity
+					.badRequest()
+					.body(new MessageResponse(errors));
+		}
+
+		// Create new user's account
+		User user = new User(signUpRequest.getUsername(),
+				signUpRequest.getEmail(),
+				signUpRequest.getName(),
+				encoder.encode(signUpRequest.getPassword()),false,null);
+
+		Set<String> strRoles = signUpRequest.getRole();
+		Set<Role> roles = new HashSet<>();
+
 		if (strRoles == null) {
 			Role userRole = roleRepository.findByName(ERole.ROLE_ETUDIANT)
 					.orElseThrow(() -> new RuntimeException("Role is not found."));
@@ -126,20 +183,16 @@ public class AuthentificationController {
 		} else {
 			strRoles.forEach(role -> {
 				switch (role) {
-				case "admin":
-				Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
-							.orElseThrow(() -> new RuntimeException("Role is not found."));
-					roles.add(adminRole);
-					break;
-				case "entreprise":
-					Role modRole = roleRepository.findByName(ERole.ROLE_ENTREPRISE)
-							.orElseThrow(() -> new RuntimeException("Role is not found."));
-					roles.add(modRole);
-					break;
-				default:
-					Role userRole = roleRepository.findByName(ERole.ROLE_ETUDIANT)
-							.orElseThrow(() -> new RuntimeException("Role is not found."));
-					roles.add(userRole);
+					case "admin":
+						Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
+								.orElseThrow(() -> new RuntimeException("Role is not found."));
+						roles.add(adminRole);
+						break;
+
+					default:
+						Role userRole = roleRepository.findByName(ERole.ROLE_ENTREPRISE)
+								.orElseThrow(() -> new RuntimeException("Role is not found."));
+						roles.add(userRole);
 				}
 			});
 		}
